7a1de75ccd9eadd92109276d6a92c505f1252cc0
Minjik06 <minjik525@deltalearns.ca>
2023-09-21 11:47:27 -0700

    fs

--- problem2.py
+++ problem2.py
@@ -3,3 +3,14 @@
 Retrieve the contents of the sd.deltasd.bc.ca webpage.
 Remove all of the HTML and display just the real contents of the page.
 """
+import requests
+import re
+data = requests.get('https://sd.deltasd.bc.ca',timeout=None)
+print(data.text)
+
+data1 = data.text
+
+html1 = re.compile('<.*?>')
+string1=re.sub(html1, '', data1)
+
+print(string1)
